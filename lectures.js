b68786ec7ef66104237e91ff869ddebeefcb3f69
jaweed <jaweedshujauddin@gmail.com>
2020-07-01 22:11:32 +0500

    done

---
+++ lectures.js
@@ -0,0 +1,10 @@
+there are three things
+Math.round
+Math.ceil
+Math.floor
+
+Math.round => round the number to nearest integer if fraction part is 0.5 or greeter then 0.5
+	      then it will round to next number like this (1.5 to 2) (1.6 to 2)
+	      if fraction is less then 0.5 then number like this (1.4 to 1) (1.1 to 1)	
+Math.ceil => forcefully round up like (1.5 to 2) (1.000001 to 2)
+Math.floor => forcefully round down like (1.5 to 1) (1.99999 to 1)
