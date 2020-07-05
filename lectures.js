94e583300a98cfdaac054c86b2d480d53061684d
jaweed <jaweedshujauddin@gmail.com>
2020-07-05 22:04:19 +0500

    done

--- lectures.js
+++ lectures.js
@@ -9,7 +9,7 @@ Math.round => round the number to nearest integer if fraction part is 0.5 or gre
 	      if fraction is less then 0.5 then number like this (1.4 to 1) (1.1 to 1)	
 Math.ceil => forcefully round up like (1.5 to 2) (1.000001 to 2)
 Math.floor => forcefully round down like (1.5 to 1) (1.99999 to 1)
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ~Lecture 27~
 Random number
 var randomNumber = Math.random();
